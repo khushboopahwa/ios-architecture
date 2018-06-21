c473c1588fbadd8040588106f97d14e4d389eb80
Khushboo Pahwa <khushboopahwa@sdnmacmini7s-mac-mini.local>
2018-06-21 09:21:54 +0530

    Initial Commit

---
+++ Lift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lift
+//
+//  Created by Khushboo Pahwa on 21/06/18.
+//  Copyright © 2018 Khushboo Pahwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
